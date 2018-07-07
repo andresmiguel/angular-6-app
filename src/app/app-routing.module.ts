e88b5faf6399379e6f0a35a80a9d56aa1e8e2b32
Andres Miguel Bores Hevia <andresmbores@gmail.com>
2018-07-07 02:11:41 -0300

    Created ShoppingList module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' }    
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,17 @@ import { Http, HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
+    HeaderComponent    
   ],
   imports: [
     BrowserModule,
@@ -27,7 +24,8 @@ import { AuthModule } from './auth/auth.module';
     HttpModule,
     RecipesModule,
     SharedModule,
-    AuthModule
+    AuthModule,
+    ShoppingListModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
