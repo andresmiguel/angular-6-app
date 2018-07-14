40338080babb529df09cde8299355981fa06f46b
Andres Miguel Bores Hevia <andresmbores@gmail.com>
2018-07-14 02:14:55 -0300

    Added router-store and store-devtools.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { CoreModule } from './core/core.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/app.reducers';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { AuthEffects } from './auth/store/auth.effects';
     CoreModule,
     HttpClientModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],  
   bootstrap: [AppComponent]
 })
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.reducers';
 import { TrySignUp } from '../store/auth.actions';
